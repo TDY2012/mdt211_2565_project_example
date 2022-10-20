1f48a44e7a3e1b674f10ac047ec14df0ae64529d
Thitiwudh Uasmith <thitiwudh.u@mail.kmutt.ac.th>
2022-10-20 17:53:24 +0700

    feat: Added example image generator.

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # mdt211_2565_project_example
+
+Install libgdiplus using `apt-get install libgdiplus`.
+
+Create `runtimeconfig.template.json` in the project directory and add these following lines:
+
+```
+{
+   "configProperties": {
+      "System.Drawing.EnableUnixSupport": true
+   }
+}
+```
+
+Add required package using `dotnet add package <package_name> --version <version>`. In this case, we added `System.Drawing.Common` version `6.0.0`.
---
+++ profile_image_generator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Drawing;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string firstName = Console.ReadLine();
+        string lastName = Console.ReadLine();
+
+        Color color = Color.FromArgb(100, Color.Gray);
+        SolidBrush brush = new SolidBrush(color);
+        Bitmap outputImage = new Bitmap(256, 256);
+
+        Graphics graphics = Graphics.FromImage(outputImage);
+        graphics.FillEllipse(brush, 0, 0, 256, 256);
+
+        outputImage.Save("test.bmp");
+        graphics.Dispose();
+    }
+}
