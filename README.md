c327e9e111d9799b28a9c0a065396368b1ff19cc
Thitiwudh Uasmith <thitiwudh.u@mail.kmutt.ac.th>
2022-10-23 18:40:06 +0700

    feat: Added international_clock project.

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Project Example
 
+## International Clock
+
+### Description
+
+This program demonstrates how the standard datetime struct and timezone info library work.
+
 ## Profile Image Generator
 
 ### Description
@@ -10,7 +16,7 @@ This program demonstrates how the standard drawing library work.
 
 Add required package to your project using `dotnet add package <package_name> --version <version>`. Please make sure you are in a project directory before calling the command. In this case, I added `System.Drawing.Common` version `6.0.0`. This depends on packages you used.
 
-### Additional Requirement for MaxOS
+### Additional Requirement for MacOS
 
 You probably need to add `runtime.osx.10.10-x64.CoreCompat.System.Drawing` version `6.0.5.128` to your project as well.
 
---
+++ international_clock/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] timezoneIdArray = new string[] {
+            "GMT Standard Time",
+            "Central America Standard Time",
+            "SE Asia Standard Time",
+            "E. South America Standard Time",
+            "AUS Eastern Standard Time",
+            "South Africa Standard Time",
+            "Tokyo Standard Time",
+        };
+
+        Console.WriteLine("Available timezone:");
+        for(int i=0; i < timezoneIdArray.Length; i++)
+        {
+            Console.WriteLine("{0}. {1}", i, timezoneIdArray[i]);
+        }
+        Console.Write("Please input a timezone id (0 - {0}): ", timezoneIdArray.Length - 1);
+
+        int timezoneIdIndex = int.Parse(Console.ReadLine());
+        if(timezoneIdIndex < 0 || timezoneIdIndex >= timezoneIdArray.Length)
+        {
+            Console.WriteLine("Invalid timezone id. Terminated.");
+        }
+
+        DateTime localDateTime = DateTime.Now;
+        DateTime convertedDateTime = TimeZoneInfo.ConvertTime(
+            localDateTime,
+            TimeZoneInfo.Local,
+            TimeZoneInfo.FindSystemTimeZoneById( timezoneIdArray[timezoneIdIndex] )
+        );
+
+        Console.WriteLine("Current time at {0} is {1}.", timezoneIdArray[timezoneIdIndex], convertedDateTime);
+    }
+}
